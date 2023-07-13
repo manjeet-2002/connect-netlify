ea2e05cae21ba963707a374dcc6a25d8cdf4eb16
Manjeet <manjeetkamboj394@gmail.com>
2023-07-14 02:32:57 +0530

    breaking of modal fixed

--- src/pages/MapFrame.js
+++ src/pages/MapFrame.js
@@ -3,11 +3,11 @@ import '../App.css';
 
 const MapFrame = (props) => {
 
-  
+  navigator.geolocation.getCurrentPosition(position=>{
     const iframeData = document.getElementById("mapFrame");
-    
+    console.log(iframeData);
     iframeData.src = `https://maps.google.com/maps?q=${props.lati},${props.longi}&hl=es;&output=embed`
-
+  })
   return (
     <div>
         <iframe id="mapFrame" height="400vh" width="100%" ></iframe>
